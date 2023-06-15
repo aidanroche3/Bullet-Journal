af0cf82d09815d6a48a186a10a8bee5f32c60e3d
EthanMoskowitz <ethanmoskowitz0@gmail.com>
2023-06-15 17:42:21 -0400

    Adding main.

---
+++ src/main/java/cs3500/pa05/Driver.java
@@ -0,0 +1,17 @@
+package cs3500.pa05;
+
+/**
+ * Main driver of the project
+ */
+public class Driver {
+
+  /**
+   * Project entry point
+   *
+   * @param args arguments for program
+   */
+  public static void main(String[] args) {
+
+  }
+
+}
